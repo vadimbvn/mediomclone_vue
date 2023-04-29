582ce44d4b4e3ff61994e823407ee2e22b3e8308
vadimbvn <sky4nii@yandex.com>
2023-04-29 23:01:01 +0300

    get currentUser

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,14 +1,19 @@
 import axios from '@/api/axios'
 
-const register = credentials => {
-  return axios.post('/users', { user: credentials })
+const register = (credentials) => {
+  return axios.post('/users', {user: credentials})
 }
 
-const login = credentials => {
-  return axios.post('/users/login', { user: credentials })
+const login = (credentials) => {
+  return axios.post('/users/login', {user: credentials})
+}
+
+const getCurrentUser = () => {
+  return axios.get('/user')
 }
 
 export default {
   register,
-  login
+  login,
+  getCurrentUser,
 }
