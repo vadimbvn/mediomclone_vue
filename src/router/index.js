6ae2249fd33f9528b1e15a2694c3828065f94f25
vadimbvn <sky4nii@yandex.com>
2023-05-02 10:56:47 +0300

    markup feed component

--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,6 @@ import Login from '@/views/Login'
 
 const routes = [
   {
-    path: '/',
-    name: 'globalFeed',
-    component: GlobalFeed,
-  },
-  {
     path: '/register',
     name: 'register',
     component: Register,
@@ -18,6 +13,51 @@ const routes = [
     path: '/login',
     name: 'login',
     component: Login,
+  },
+  {
+    path: '/',
+    name: 'globalFeed',
+    component: GlobalFeed,
+  },
+  {
+    path: '/feed',
+    name: 'yourFeed',
+    component: GlobalFeed,
+  },
+  {
+    path: '/tags/:slug',
+    name: 'tag',
+    component: GlobalFeed,
+  },
+  {
+    path: '/articles/new',
+    name: 'createArticle',
+    component: GlobalFeed,
+  },
+  {
+    path: '/articles/:slug',
+    name: 'article',
+    component: GlobalFeed,
+  },
+  {
+    path: '/articles/:slug/edit',
+    name: 'editArticle',
+    component: GlobalFeed,
+  },
+  {
+    path: '/settings',
+    name: 'settings',
+    component: GlobalFeed,
+  },
+  {
+    path: '/profiles/:slug',
+    name: 'userProfile',
+    component: GlobalFeed,
+  },
+  {
+    path: '/profiles/:slug/favorites',
+    name: 'userProfileFavorites',
+    component: GlobalFeed,
   },
 ]
 
