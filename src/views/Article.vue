b34d6ba36046d31d42af20108bb5321a041ad084
vadimbvn <sky4nii@yandex.com>
2023-06-22 17:04:59 +0300

    add computed isAuthor

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -20,7 +20,7 @@
             </router-link>
             <span class="date">{{ article.createdAt }}</span>
           </div>
-          <span>
+          <span v-if="isAuthor">
             <router-link
               class="btn btn-outline-secondary btn-sm"
               :to="{name: 'editArticle', params: {slug: article.slug}}"
@@ -52,20 +52,36 @@
 </template>
 
 <script>
-import {actionTypes} from '@/store/modules/article'
-import {mapState} from 'vuex'
+import {actionTypes as articleActionTypes} from '@/store/modules/article'
+import {getterTypes as authGetterTypes} from '@/store/modules/auth'
+import {mapState, mapGetters} from 'vuex'
+import McvLoading from '@/components/Loading.vue'
+import McvErrorMessage from '@/components/ErrorMessage.vue'
 
 export default {
   name: 'McvArticle',
+  components: {
+    McvLoading,
+    McvErrorMessage,
+  },
   computed: {
     ...mapState({
       isLoading: (state) => state.article.isLoading,
       article: (state) => state.article.data,
       error: (state) => state.article.error,
     }),
+    ...mapGetters({
+      currentUser: authGetterTypes.currentUser,
+    }),
+    isAuthor() {
+      if (!this.currentUser || !this.article) {
+        return false
+      }
+      return this.currentUser.username === this.article.author.username
+    },
   },
   mounted() {
-    this.$store.dispatch(actionTypes.getArticle, {
+    this.$store.dispatch(articleActionTypes.getArticle, {
       slug: this.$route.params.slug,
     })
   },
